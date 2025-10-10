ac60ee31d9b23db2c153ee697439e5ead2618cbc
jochris15 <jochris15@gmail.com>
2025-10-10 12:40:11 +0700

    updated

--- end/src/views/BaseLayout.jsx
+++ end/src/views/BaseLayout.jsx
@@ -1,8 +1,23 @@
 import { Navigate, Outlet } from "react-router";
 import Navbar from "../components/Navbar";
+import Toastify from 'toastify-js'
 
 export default function BaseLayout() {
+    // protecting routes
     if (!localStorage.access_token) {
+        Toastify({
+            text: "Please login first",
+            duration: 3000,
+            newWindow: true,
+            close: true,
+            gravity: "bottom", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+                background: "#F87171",
+                color: "#000000"
+            }
+        }).showToast();
         return <Navigate to="/login" />
     }
 
--- end/src/views/LoginPage.jsx
+++ end/src/views/LoginPage.jsx
@@ -46,7 +46,21 @@ export default function LoginPage() {
         }
     }
 
+    // protecting routes
     if (localStorage.access_token) {
+        Toastify({
+            text: "You already logged in",
+            duration: 3000,
+            newWindow: true,
+            close: true,
+            gravity: "bottom", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+                background: "#F87171",
+                color: "#000000"
+            }
+        }).showToast();
         return <Navigate to="/" />
     }
 
